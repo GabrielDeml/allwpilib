bbdce3d7c1b79b9113b6a4ef91f8aed81caa8923
GabrielDeml <gabrielddeml@gmail.com>
2021-01-07 10:28:35 -0500

    added comments

--- simulation/halsim_gui/src/main/native/cpp/NetworkTablesSimGui.cpp
+++ simulation/halsim_gui/src/main/native/cpp/NetworkTablesSimGui.cpp
@@ -20,12 +20,17 @@ static std::unique_ptr<glass::NetworkTablesView> gNetworkTablesView;
 static glass::Window* gNetworkTablesWindow;
 
 void NetworkTablesSimGui::Initialize() {
+  // Create gNetworkTablesModel object
   gNetworkTablesModel = std::make_unique<glass::NetworkTablesModel>();
+  // Create the view object
   gNetworkTablesView =
       std::make_unique<glass::NetworkTablesView>(gNetworkTablesModel.get());
+  // Make update be called each frame
   wpi::gui::AddEarlyExecute([] { gNetworkTablesModel->Update(); });
+  // Add window to HALSimGui
   gNetworkTablesWindow = HALSimGui::ntProvider.AddWindow(
       "NetworkTables", [] { gNetworkTablesView->Display(); });
+  // Display the network tables window
   if (gNetworkTablesWindow) {
     gNetworkTablesWindow->SetDefaultPos(250, 277);
     gNetworkTablesWindow->SetDefaultSize(750, 185);
